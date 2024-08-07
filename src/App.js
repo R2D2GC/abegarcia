717922d892fbdc99274549a1e6818d56fe989f19
R2D2GC <118461305+R2D2GC@users.noreply.github.com>
2024-08-07 14:45:04 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Navbar } from './components/MuiNavbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MuiNavbar.js
@@ -0,0 +1,42 @@
+  import { AppBar, Toolbar, IconButton, Typography, Slide} from "@mui/material"
+  import { CatchingPokemon } from "@mui/icons-material"
+  import React, { useState, useEffect} from "react"
+
+  export const Navbar = () => {
+    const [showNavbar, setShowNavbar] = useState (true);
+
+    useEffect(() =>{
+      let lastScrollY = window.scrollY;
+
+    const handleScroll = () => {
+      if (window.scrollY > lastScrollY) {
+        // Scrolling down
+        setShowNavbar(false);
+      } else {
+        // Scrolling up
+        setShowNavbar(true);
+      }
+      lastScrollY = window.scrollY;
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []
+)
+    return (
+      <Slide in={showNavbar} direction="down">
+      <AppBar position="static">
+        <Toolbar> 
+          <IconButton edge='start' color="inherit" aria-label="logo">
+            <CatchingPokemon/>
+          </IconButton>
+          <Typography variant="h6" component='div'>
+            Abe Garcia  
+        </Typography> 
+        </Toolbar> 
+      </AppBar>
+      </Slide>
+    )
+  }
